9cd89b22013b7d1b37459342e141feb299d4a4ab
Gwhiite <guibrancodecampos@gmail.com>
2024-11-30 03:21:28 +0000

    feat: todo final

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todo Web App</h1>
+    <div class="input-container">
+      <input
+        @keyup.enter="handleClick"
+        type="text"
+        placeholder="Digite seu afazer"
+        v-model="value" />
+      <span @click="handleClick">+</span>
+    </div>
+
+    <TodoItem :afazer="todos" @remove-todo="receiveEmit" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoItem from "./components/TodoItem.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { TodoItem },
+  name: "App",
+  data() {
+    return {
+      value: "",
+      todos: [],
+    };
+  },
+  methods: {
+    receiveEmit(todo) {
+      this.todos.splice(todo, 1);
+      console.log(this.todos);
+    },
+    handleClick() {
+      this.todos.push(this.value);
+      this.value = "";
+    },
+  },
+};
 </script>
 
 <style>
+body {
+  background-color: #242424;
+  color: white;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 700px;
+  margin: 0 auto;
+  font-family: sans-serif;
+}
+
+input {
+  min-width: 300px;
+  max-width: 300px;
+  font-size: 1rem;
+  padding: 10px;
+  border: 2px solid #42b883;
+  border-radius: 8px;
+  background-color: #242424;
+  color: #42b883;
+}
+
+input:focus {
+  outline: none;
+}
+
+.input-container {
+  display: flex;
+  align-items: center;
+}
+
+.input-container span {
+  margin-left: 10px;
+  font-size: 2rem;
+  color: #42b883;
+  cursor: pointer;
 }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="todo-container" v-if="afazer.length > 0">
+      <div class="todo" v-for="(todo, index) in afazer" :key="index">
+        <p>{{ todo }}</p>
+        <span class="remove" @click="removeTodo(index)">remove</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["afazer"],
+  methods: {
+    removeTodo(todo) {
+      this.$emit("remove-todo", todo);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.todo-container {
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+  gap: 20px;
+}
+
+.todo {
+  border: 2px solid #42b883;
+  padding: 10px 20px;
+  max-width: 400px;
+  min-width: 400px;
+  font-size: 1rem;
+  border-radius: 8px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.remove {
+  cursor: pointer;
+  color: red;
+}
+</style>
